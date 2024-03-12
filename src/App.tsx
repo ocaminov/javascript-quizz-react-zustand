67131f7c507e313897b2e8751170c2bdffca6066
ocaminov <ocaminov@gmail.com>
2024-03-12 12:00:04 -0400

    Javascript Quizz con React y Zustand

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Container, Stack, Typography } from '@mui/material'
+import './App.css'
+import { JavaScriptLogo } from './JavaScriptLogo'
+import { Start } from './Start'
+import { useQuestionStore } from './store/questions'
+import { Game } from './Game'
+
+function App() {
+  const questions = useQuestionStore(state => state.questions)
+
+  return (
+    <main>
+      <Container maxWidth='sm'>
+      <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+          <JavaScriptLogo />
+          <Typography variant='h2' component='h1'>
+          Javascript Quizz
+          
+        
+        </Typography>
+        </Stack>
+        {questions.length === 0 && <Start />}
+        {questions.length > 0 && <Game />}
+      </Container>
+      
+    </main>
+  )
+}
+
+export default App
---
+++ src/Game.tsx
@@ -0,0 +1,85 @@
+import {
+  Card,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useQuestionStore } from "./store/questions";
+import { type Question as QuestionType } from "./types";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { ArrowBackIosNew, ArrowForwardIos } from "@mui/icons-material";
+import { Footer } from "./Footer";
+
+const getBackgroundColor = (info: QuestionType, index: number) => {
+  const {userSelectedAnswer, correctAnswer} = info
+  //usuario no ha seleccionado nada todavia
+  if(userSelectedAnswer == null) return 'transparent'
+  //si ya selecciono pero la solucion es incorrecta
+  if(index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+  //si esta es la solucion correcta
+  if(index === correctAnswer) return 'green'
+  //si esta es la seleccion del usuario pero no es correcta
+  if(index === userSelectedAnswer) return 'red'
+  //si no es ninguna de las anteriores
+  return 'transparent'
+}
+
+const Question = ({ info }: { info: QuestionType }) => {
+  const selectAnswer = useQuestionStore((state) => state.selectAnswer);
+  const createHandleClick = (answerIndex: number) => () => {
+    selectAnswer(info.id, answerIndex);
+  };
+
+  
+
+  return (
+    <Card
+      variant="outlined"
+      sx={{ bgcolor: "#222", p: 2, textAlign: "left", marginTop: 4 }}
+    >
+      <Typography variant="h5">{info.question}</Typography>
+      <SyntaxHighlighter language="javascript" style={gradientDark}>
+        {info.code}
+      </SyntaxHighlighter>
+      <List sx={{ bgcolor: "#333" }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton onClick={createHandleClick(index)} sx={{backgroundColor: getBackgroundColor(info,index)}} disabled={info.userSelectedAnswer != null}>
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Card>
+  );
+};
+
+export const Game = () => {
+  const questions = useQuestionStore((state) => state.questions);
+  const currentQuestion = useQuestionStore((state) => state.currentQuestion);
+  const goNextQuestion = useQuestionStore((state) => state.goNextQuestion)
+  const goPreviousQuestion = useQuestionStore((state) => state.goPreviousQuestion)
+  const questionInfo = questions[currentQuestion];
+  return (
+    <>
+    <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+      <IconButton onClick={goPreviousQuestion} disabled={currentQuestion === 0}>
+        <ArrowBackIosNew />
+      </IconButton>
+
+      {currentQuestion + 1} / {questions.length}
+
+      <IconButton onClick={goNextQuestion} disabled={currentQuestion > questions.length - 1}>
+        <ArrowForwardIos />
+      </IconButton>
+    </Stack>
+      <Question info={questionInfo} />
+      <Footer />
+    </>
+  );
+};
